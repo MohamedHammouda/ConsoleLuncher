267ff14b473ad9e9807c36aa58ae5ddb9801284f
mohamed <med_hammouda@yahoo.fr>
2021-10-24 10:17:30 +0100

    first commit for our project

---
+++ src/test/java/FirstUnitTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstUnitTest {
+
+    @Test
+    public void whenThis_thenThat() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whenSomething_thenSomething() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whenSomethingElse_thenSomethingElse() {
+        assertTrue(true);
+    }
+}
